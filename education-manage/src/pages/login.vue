58eebe7514f2f74a2dc438074f8ec1d954aa3ddb
wanglu <wanglu@able-elec.com>
2020-11-23 23:36:46 +0800

    路由修改

---
+++ education-manage/src/pages/login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="login">
+    登录
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      userName: '',
+      password: ''
+    }
+  },
+  methods: {
+    login () {
+
+    }
+  }
+}
+</script>
+
+<style lang="less">
+</style>
