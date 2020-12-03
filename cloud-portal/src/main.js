8162364ae9b5dcc92fc51148b9e624fb0c0724a4
wanglu <wanglu@able-elec.com>
2020-12-03 10:36:18 +0800

    引入element-ui组件

--- cloud-portal/src/main.js
+++ cloud-portal/src/main.js
@@ -3,8 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { Message, Loading, MessageBox } from "element-ui"
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
+Vue.prototype.$message = Message;
+Vue.prototype.$confirm = MessageBox.confirm;
+Vue.prototype.$loading = Loading.service;
 
 /* eslint-disable no-new */
 new Vue({
