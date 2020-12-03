cc36abcf32b0396d12f9e90361cd2bfd0d97acd8
wanglu <wanglu@able-elec.com>
2020-12-03 10:51:01 +0800

    项目初始化

---
+++ cloud-portal/src/components/portal.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="portal">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Portal',
+  data () {
+    return {
+    
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.portal{
+  width: 100%;
+  height:100%;
+}
+</style>
