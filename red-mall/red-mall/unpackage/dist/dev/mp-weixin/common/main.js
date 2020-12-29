55fbf868c3ef70909222e7300a30417d71a43bc1
wanglu <wanglu@able-elec.com>
2020-12-29 10:05:39 +0800

    red-mall

---
+++ red-mall/red-mall/unpackage/dist/dev/mp-weixin/common/main.js
@@ -0,0 +1,13 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["common/main"],[
+/* 0 */
+/*!*********************************************************!*\
+  !*** D:/code/project-summary/red-mall/red-mall/main.js ***!
+  \*********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/@dcloudio/webpack-uni-mp-loader/lib/main.js):\nError: ENOENT: no such file or directory, open 'D:\\code\\project-summary\\red-mall\\red-mall\\main.js'");
+
+/***/ })
+],[[0,"common/runtime"]]]);
+//# sourceMappingURL=../../.sourcemap/mp-weixin/common/main.js.map
---
+++ red-mall/red-mall/unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -0,0 +1,15 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["pages/index/index"],{
+
+/***/ 23:
+/*!******************************************************************************************!*\
+  !*** D:/code/project-summary/red-mall/red-mall/main.js?{"page":"pages%2Findex%2Findex"} ***!
+  \******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/@dcloudio/webpack-uni-mp-loader/lib/main.js):\nError: ENOENT: no such file or directory, open 'D:\\code\\project-summary\\red-mall\\red-mall\\main.js'");
+
+/***/ })
+
+},[[23,"common/runtime"]]]);
+//# sourceMappingURL=../../../.sourcemap/mp-weixin/pages/index/index.js.map
