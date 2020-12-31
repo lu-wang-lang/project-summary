eb4f24203d841e75c525fa82be7d2106ee93dce4
wanglu <wanglu@able-elec.com>
2020-12-31 18:46:23 +0800

    商品详情页面大家

--- red-mall/pages/category/detail.vue
+++ red-mall/pages/category/detail.vue
@@ -1,6 +1,9 @@
 <template>
   <view class="good-detail-container">
-    商品信息
+    <view class="main-container">
+
+    </view>
+    <view class="footer"></view>
   </view>
 </template>
 
@@ -19,5 +22,17 @@ export default {
 .good-detail-container{
   width: 100vw;
   height: 100vh;
+  display: flex;
+  flex-direction: column;
+  .main-container{
+    width: 100%;
+    flex:1;
+    overflow-y: auto;
+  }
+  .footer{
+    width: 100%;
+    height: 100upx;
+    border:1px solid #eee;
+  }
 }
 </style>
